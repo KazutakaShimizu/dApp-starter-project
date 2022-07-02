b9615dd2fd81e00ad953b489868c8028db2f3a0a
KazutakaShimizu <s.kazutaka55555@gmail.com>
2022-07-02 20:56:34 +0900

    tweak

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,7 @@ const App = () => {
       const accounts = await ethereum.request({
         method: 'eth_requestAccounts',
       })
+
       console.log('Connected: ', accounts[0])
       setCurrentAccount(accounts[0])
     } catch (error) {
